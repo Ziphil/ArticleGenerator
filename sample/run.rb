7896bd3938cfbbf11fc75ceb0884cd2e3385ba7d
Ziphil <yfe23457@nifty.com>
2018-12-27 18:49:56 +0900

    サンプルのライブラリのURLをCDNに変更

--- sample/run.rb
+++ sample/run.rb
@@ -8,9 +8,9 @@ $stdout.sync = true
 
 
 option = {
-  :mathjax_directory => "mathjax",
-  :vis_directory => "vis",
-  :jquery_path => "jquery/jquery.js",
+  :mathjax_directory => "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5",
+  :vis_directory => "https://cdnjs.cloudflare.com/ajax/libs/vis/4.21.0",
+  :jquery_path => "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js",
   :comprehensive => true,
   :show_name => false,
   :show_popup_number => false
